5cd293b9c38ebf540830781d5f9cc432ff3ba7d4
tinny <tinnystudios@gmail.com>
2018-04-28 13:24:17 +1000

    Commiting new script

---
+++ Inheritancy/Inheritancy/Assets/NewScript.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour {
+    //This is a new script, nothing has been added yet
+}
