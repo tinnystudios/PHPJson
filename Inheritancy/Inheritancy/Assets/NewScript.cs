3a3949d60433a779498d9c1712e3c0afdc87b3e3
tinny <tinnystudios@gmail.com>
2018-04-28 13:31:55 +1000

    Made modification to new script inside test branch

--- Inheritancy/Inheritancy/Assets/NewScript.cs
+++ Inheritancy/Inheritancy/Assets/NewScript.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewScript : MonoBehaviour {
-    //This is a new script, nothing has been added yet
+    //This is a new script inside TEST BRANCH
 }
